3c8cb389e89e2274817126ca4c740537146877dd
tparker <tparker@usgs.gov>
2017-09-07 16:08:02 -0800

    getmenu

--- getmenu.py
+++ getmenu.py
@@ -7,14 +7,21 @@ host = 'pubavo1.wr.usgs.gov'
 port = 16023
 
 s.connect((host, port))
-s.send("MENU: GS SCNL\n")
+s.send("MENU: 0 SCNL\n")
 char = ''
 str = ''
 while char != '\n':
     old_char = char
     char = s.recv(1);
     if old_char == ' ' and char == ' ':
-        print(str)
+        #print(str)
+        words = str.split(" " )
+        if len(words) > 2:
+            sid = int(words[0])
+            st = float(words[5])
+            et = float(words[6])
+            if et <= st:
+                print("found: " + str)
         str = ''
     else:
         str += char
